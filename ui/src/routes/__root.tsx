50fd9b23ef0df1ee0c95f2291eb794971fc0ac88
MarcelArt <badman.islemon@gmail.com>
2025-06-11 23:21:05 +0700

    feat - initialize login page

--- ui/src/routes/__root.tsx
+++ ui/src/routes/__root.tsx
@@ -1,12 +1,11 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
-import Header from '../components/Header'
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Header />
+      {/* <Header /> */}
 
       <Outlet />
       <TanStackRouterDevtools />
