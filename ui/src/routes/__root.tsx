a3698c6a485f3a8cd53e72cdabd2581601682095
MarcelArt <badman.islemon@gmail.com>
2025-06-12 00:35:14 +0700

    feat - theming

--- ui/src/routes/__root.tsx
+++ ui/src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from '@/components/app-sidebar';
+import { ThemeProvider } from '@/components/theme-provider';
 import { Separator } from '@/components/ui/separator';
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { Outlet, createRootRoute, useLocation } from '@tanstack/react-router';
@@ -12,14 +13,15 @@ export const Route = createRootRoute({
 
 		return (
 			<>
-				{!shouldHideSidebar ? (
-					<SidebarProvider>
-						<AppSidebar />
-						<SidebarInset>
-							<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-								<SidebarTrigger className="-ml-1" />
-								<Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
-								{/* <Breadcrumb>
+				<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+					{!shouldHideSidebar ? (
+						<SidebarProvider>
+							<AppSidebar />
+							<SidebarInset>
+								<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+									<SidebarTrigger className="-ml-1" />
+									<Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+									{/* <Breadcrumb>
             <BreadcrumbList>
               <BreadcrumbItem className="hidden md:block">
                 <BreadcrumbLink href="#">
@@ -32,9 +34,9 @@ export const Route = createRootRoute({
               </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb> */}
-							</header>
-							<Outlet />
-							{/* <div className="flex flex-1 flex-col gap-4 p-4">
+								</header>
+								<Outlet />
+								{/* <div className="flex flex-1 flex-col gap-4 p-4">
 						<div className="grid auto-rows-min gap-4 md:grid-cols-3">
 							<div className="bg-muted/50 aspect-video rounded-xl" />
 							<div className="bg-muted/50 aspect-video rounded-xl" />
@@ -42,11 +44,12 @@ export const Route = createRootRoute({
 						</div>
 						<div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
 					</div> */}
-						</SidebarInset>
-					</SidebarProvider>
-				) : (
-					<Outlet />
-				)}
+							</SidebarInset>
+						</SidebarProvider>
+					) : (
+						<Outlet />
+					)}
+				</ThemeProvider>
 
 				<TanStackRouterDevtools />
 			</>
